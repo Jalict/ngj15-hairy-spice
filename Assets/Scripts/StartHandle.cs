bfbcdb6596f8a3a97edcb6387cfa2cce80fcc4d9
Frans Peter Larsen <larsen.frans@gmail.com>
2015-02-07 09:24:50 +0100

    Initial Spriter2Unity

---
+++ Assets/Scripts/StartHandle.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartHandle : MonoBehaviour {
+	float timestamp;
+	int playersReady;
+
+	// Use this for initialization
+	void Start () {
+		timestamp = Time.realtimeSinceStartup;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject[] players = GameObject.FindGameObjectsWithTag ("Player");
+
+		playersReady = 0;
+
+		for(int i = 0; i < players.Length;i++) {
+			if(players[i].GetComponent<PlayerSelect>().isReady) {
+				playersReady++;
+			}
+		}
+
+		bool isTwoPlayersReady = playersReady >= 2;
+
+		if (Input.GetButtonDown("X_1") && isTwoPlayersReady) {
+			timestamp = Time.realtimeSinceStartup;
+
+			//if(isTwoPlayersReady) {
+				Application.LoadLevel("Frans-main");
+			//}
+		}
+
+	}
+
+	void OnGUI() {
+		GUI.Label (new Rect (10, 10, 100, 30), (20 - timestamp)+"");
+		GUI.Label (new Rect (10, 40, 30, 30), ""+playersReady);
+	}
+}
