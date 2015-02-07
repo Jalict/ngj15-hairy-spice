69dadaf43b7d8f58b7e9b5b173f3011f6aae25db
Frans Peter Larsen <larsen.frans@gmail.com>
2015-02-07 02:08:00 +0100

    Basic movement (All controllers) Based on http://wiki.unity3d.com/index.php?title=Xbox360Controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public int xboxController;
+
+	CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Check for left, right movement
+		float x = Input.GetAxis ("L_XAxis_" + (xboxController + 1)) * 80;
+
+		controller.SimpleMove(new Vector3 (x , 0, 0) * Time.deltaTime);
+
+		// Jumping
+		if(Input.GetButton("A_"+ (xboxController+1))) {
+			controller.SimpleMove(new Vector3 (0, 20, 0) * Time.deltaTime);
+		}
+	}
+}
