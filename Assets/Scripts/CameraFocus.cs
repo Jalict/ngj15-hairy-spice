247da78e233f6f4c94fbe5dad54d80181fc09110
Frans Peter Larsen <larsen.frans@gmail.com>
2015-02-07 01:35:33 +0100

    Initial Movement

---
+++ Assets/Scripts/CameraFocus.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFocus : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// TODO Find out how many players are connected
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// TODO Some lerping
+	}
+}
