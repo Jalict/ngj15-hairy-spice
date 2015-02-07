d028c876bb26fbd3fb8cf68113aa6396a448c23b
Frans Peter Larsen <larsen.frans@gmail.com>
2015-02-07 03:02:40 +0100

    Movement - Handing over to Casper for gravity, speed and handling

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,24 +3,54 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 	public int xboxController;
+	public float jumpSpeed;
+	public float gravity;
+	public float movementSpeed;
+
+	bool isDead;
+	public Vector3 movement;
 
 	CharacterController controller;
 
-	// Use this for initialization
+
+	// Use this for initialization 
 	void Start () {
 		controller = GetComponent<CharacterController> ();
+
+		isDead = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// Check for left, right movement
-		float x = Input.GetAxis ("L_XAxis_" + (xboxController + 1)) * 80;
-
-		controller.SimpleMove(new Vector3 (x , 0, 0) * Time.deltaTime);
+		movement.y -= gravity * Time.deltaTime; 
 
 		// Jumping
-		if(Input.GetButton("A_"+ (xboxController+1))) {
-			controller.SimpleMove(new Vector3 (0, 20, 0) * Time.deltaTime);
+		if(Input.GetButtonDown("A_"+ (xboxController+1))) {
+			movement.y = jumpSpeed;
+
 		}
+
+		// Check for left, right movement
+		movement.x = Input.GetAxis ("L_XAxis_" + (xboxController + 1)) * movementSpeed;
+		
+		controller.Move(movement * Time.deltaTime);
+
+		// Check death, respawn
+		if (isDead) {
+			// Get list of respawn positions for player spawning
+			// TODO: Make sure that the respawn positions haven't just been used
+			GameObject[] respawnPositions = GameObject.FindGameObjectsWithTag("Respawn");
+
+			// Just choose a random one (Bad lol)
+			transform.position = respawnPositions[Random.Range(0,respawnPositions.Length)].transform.position;
+
+			isDead = false;
+		}
+
+
+	}
+
+	public void Kill() {
+		isDead = true;
 	}
 }
