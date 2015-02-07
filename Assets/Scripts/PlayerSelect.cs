a740e8cc51750e62ba4b7a8b47aa8e07d1dbcc3e
Frans Peter Larsen <larsen.frans@gmail.com>
2015-02-07 03:37:19 +0100

    Initial Player Selection Screen

---
+++ Assets/Scripts/PlayerSelect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSelect : MonoBehaviour {
+	public int xboxController;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+		if(Input.GetButton("A_" + (xboxController+1))) {
+			renderer.material.color = Color.red;
+		} else {
+			renderer.material.color = Color.white;
+		}
+	}
+}
