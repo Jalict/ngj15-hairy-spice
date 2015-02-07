b732e5641d794b947d3f3e69008b6a9b46c55d95
Piceh <phil_1991@hotmail.com>
2015-02-07 17:40:16 +0100

    basic health system 100 hp, nothing happens when hit 0, can go below at the moment

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	public int health = 100;
+	public int DmgTakeScale = 1;
+
+
+	void OnCollisionEnter(Collision collision){
+
+		print("Collision!");
+
+		if(collision.gameObject.tag == "enemy" && collision.relativeVelocity.magnitude > 0){
+
+			health -= (int)(collision.relativeVelocity.magnitude * DmgTakeScale);
+
+		}
+
+	}
+
+}
