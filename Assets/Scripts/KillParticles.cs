2fb90a18afc569062aecfa85b9e852acfdf5d645
Frans Peter Larsen <larsen.frans@gmail.com>
2015-02-08 10:54:29 +0100

    Kill script

---
+++ Assets/Scripts/KillParticles.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillParticles : MonoBehaviour {
+    ParticleSystem p;
+
+	// Use this for initialization
+	void Start () {
+        p = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (p.time > 2)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
