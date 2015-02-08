270645b433b639a18e8a30c87ec794d20a0f9baf
Frans Peter Larsen <larsen.frans@gmail.com>
2015-02-08 10:36:29 +0100

    Added some missing stuff (Teleporter) Added explosion stuff for impact

---
+++ Assets/Scripts/PulseLight.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PulseLight : MonoBehaviour {
+    float n;
+
+	// Use this for initialization
+	void Start () {
+        n = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        n += .3f;
+        //GetComponent<Light>().intensity = (Mathf.Sin(n)*1.5f)+4;
+        GetComponent<Light>().range = (Mathf.Cos(n) * 0.5f) + 3;
+	}
+}
